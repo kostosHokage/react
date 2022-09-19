7c1daac7700338b645df72df7d0e32b30bc2582b
kostos <putivcev123@gmail.com>
2022-09-19 20:07:19 +0300

    #4 добавил классовый компонент Square.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css"
+import Square from "./Square";
 
 function App() {
   return (
     <div className="App">
-        <div className="square"></div>
+      <Square />
     </div>
   );
 }
---
+++ src/Square.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import "./App.css"
+
+class Square extends Component {
+    render() {
+        return (
+            <div className="square">
+                <h1>Square</h1>
+            </div>
+        );
+    }
+}
+
+export default Square;
