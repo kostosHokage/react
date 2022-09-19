4dcaabb9277ef63e8373dac104ee876a25643efe
kostos <putivcev123@gmail.com>
2022-09-19 19:43:11 +0300

    #4 added style App.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-        <h1>Тестовое сообщение</h1>
+        <div className="square"></div>
     </div>
   );
 }
