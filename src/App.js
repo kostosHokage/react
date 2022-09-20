2c1b79d48b3ca960bb2aa98be164918f5289448c
Konstantin <95746396+kostosHokage@users.noreply.github.com>
2022-09-20 19:55:08 +0300

    #4 added style App.css (#7)
    * #4 added style App.css
    
    * #4 добавил классовый компонент Square.js
    
    Co-authored-by: kostos <putivcev123@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./App.css"
+import Square from "./Square";
 
 function App() {
   return (
     <div className="App">
-        <h1>Тестовое сообщение</h1>
+      <Square />
     </div>
   );
 }
