8d81b05901587d80cb75af1f65ceb01077d546e9
kostos <putivcev123@gmail.com>
2022-09-21 22:00:56 +0300

    #5 создал массив с 10 элементами, с помощью перебора map вывел каждый элемент в div

--- src/Square.js
+++ src/Square.js
@@ -3,20 +3,25 @@ import "./App.css"
 
 export default function Square(props) {
 
-    let objects = [
-        {name: 'Hello', id: 1},
-        {name: 'Hello', id: 2},
-        {name: 'Hello', id: 3},
-        {name: 'Hello', id: 4},
-    ]
+let objects = [
+    {name: 'Hello', id: 1},
+    {name: 'Sasha', id: 2},
+    {name: 'Kostya', id: 3},
+    {name: 244, id: 4},
+    {name: 'Vanya', id: 5},
+    {name: 'Pasha', id: 6},
+    {name: 'Egor', id: 7},
+    {name: 'Seniors', id: 8},
+    {name: 'React', id: 9},
+    {name: 'Developers', id: 10},
+]
 
 
     return (
-
-        <div className="square">
-            {props.objects.map(el => <li id={el.id}>{el.name}</li>)}
+        <div className="container">
+            {objects.map(el => <div className="square" key={el.id}>{el.name}</div>)}
         </div>
 
-)
+    )
 }
 
