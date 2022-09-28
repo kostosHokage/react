1284f02ffe7b350087b98d32967e94be44ffaf13
kostos <putivcev123@gmail.com>
2022-09-28 23:25:44 +0300

    исправил ошибки, довел линтер до идеала, двигаюсь дальше

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,26 +1,49 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
 
-export default function Square(props) {
+export default function Square() {
+  const objects = [
+    { id: 1, value: 1.2, color: "yellow" },
+    { id: 2, value: "Sasha", color: "white" },
+    { id: 3, value: "Kostya", color: "black" },
+    { id: 4, value: 244, color: "grey" },
+    { id: 5, value: 222, color: "yellow" },
+    { id: 6, value: "Pasha", color: "red" },
+    { id: 7, value: "Egor", color: "blue" },
+    { id: 8, value: 10.002, color: "pink" },
+    { id: 9, value: 12, color: "borrow" },
+    { id: 10, value: "Developers", color: "black" },
+  ];
 
-    let objects = [
-        {id: 1, value: 1.2, color: "yellow"},
-        {id: 2, value: 'Sasha', color: "white"},
-        {id: 3, value: 'Kostya', color: "black"},
-        {id: 4, value: 244, color: "grey"},
-        {id: 5, value: 222, color: 'yellow'},
-        {id: 6, value: 'Pasha', color: "red"},
-        {id: 7, value: 'Egor', color: "blue"},
-        {id: 8, value: 10.002, color: "pink"},
-        {id: 9, value: 12, color: "borrow"},
-        {id: 10, value: 'Developers', color: "black"},
-    ]
+  const objectsCopy = [
+    { id: 1, value: 1 },
+    { id: 2, value: 12 },
+    { id: 3, value: 2 },
+    { id: 4, value: 1 },
+    { id: 5, value: 4 },
+    { id: 6, value: 0 },
+    { id: 7, value: 11 },
+    { id: 8, value: 2 },
+    { id: 9, value: 7 },
+    { id: 10, value: 6 },
+  ];
 
-    return (
-        <div className="container">
-            {objects.map(el => <div className="square" style={{backgroundColor: el.color}} key={el.id}>{el.value}</div>)}
+  return (
+    <div className="container">
+      {objects.map((el) => (
+        <div
+          className="square"
+          style={{ backgroundColor: el.color }}
+          key={el.id}
+        >
+          {el.value}
         </div>
-
-    )
+      ))}
+      {objectsCopy.map((el) => (
+        <div className="square" key={el.id}>
+          {el.value}
+        </div>
+      ))}
+    </div>
+  );
 }
-
