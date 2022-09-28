02099348f65941cf076b1d4273deb801e618868f
kostos <putivcev123@gmail.com>
2022-09-28 16:05:56 +0300

    Revert "#13 Переустановил эслинтер и притир, так же продублировал массив изменив чутка его содержание"
    This reverts commit f6b7d3a5e3fa28d791b09267a18c930aa3fb05cf.

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,43 +1,26 @@
 import React from "react";
-import "./App.css";
+import "./App.css"
 
-export default function Square() {
-  const objects = [
-    { id: 1, value: 1.2, color: "yellow" },
-    { id: 2, value: "Sasha", color: "white" },
-    { id: 3, value: "Kostya", color: "black" },
-    { id: 4, value: 244, color: "grey" },
-    { id: 5, value: 222, color: "yellow" },
-    { id: 6, value: "Pasha", color: "red" },
-    { id: 7, value: "Egor", color: "blue" },
-    { id: 8, value: 10.002, color: "pink" },
-    { id: 9, value: 12, color: "borrow" },
-    { id: 10, value: "Developers", color: "black" },
-  ];
-  // let objectsCopy = [
-  //     {id: 1, value: 12},
-  //     {id: 2, value: 1},
-  //     {id: 3, value: 2},
-  //     {id: 4, value: 3},
-  //     {id: 5, value: 4.3},
-  //     {id: 6, value: 121},
-  //     {id: 7, value: 33},
-  //     {id: 8, value: 8},
-  //     {id: 9, value: 12},
-  //     {id: 10, value: 9},
-  // ]
+export default function Square(props) {
 
-  return (
-    <div className="container">
-      {objects.map((el) => (
-        <div
-          className="square"
-          style={{ backgroundColor: el.color }}
-          key={el.id}
-        >
-          {el.value}
+    let objects = [
+        {id: 1, value: 1.2, color: "yellow"},
+        {id: 2, value: 'Sasha', color: "white"},
+        {id: 3, value: 'Kostya', color: "black"},
+        {id: 4, value: 244, color: "grey"},
+        {id: 5, value: 222, color: 'yellow'},
+        {id: 6, value: 'Pasha', color: "red"},
+        {id: 7, value: 'Egor', color: "blue"},
+        {id: 8, value: 10.002, color: "pink"},
+        {id: 9, value: 12, color: "borrow"},
+        {id: 10, value: 'Developers', color: "black"},
+    ]
+
+    return (
+        <div className="container">
+            {objects.map(el => <div className="square" style={{backgroundColor: el.color}} key={el.id}>{el.value}</div>)}
         </div>
-      ))}
-    </div>
-  );
+
+    )
 }
+
