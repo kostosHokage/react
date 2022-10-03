c302980d76a9f74ab78e8f999ff9b3f5c259ef46
kostos <putivcev123@gmail.com>
2022-10-03 21:27:47 +0300

    #11 Переделал вывод элемента в функциональный

--- src/Square.jsx
+++ src/Square.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function Square() {
-  const objects = [
-    { id: 1, value: 1.2, color: "yellow" },
-    { id: 2, value: "Sasha", color: "white" },
-    { id: 3, value: "Kostya", color: "green" },
-    { id: 4, value: 244, color: "grey" },
-    { id: 5, value: 222, color: "yellow" },
-    { id: 6, value: "Pasha", color: "red" },
-    { id: 7, value: "Egor", color: "blue" },
-    { id: 8, value: 10.002, color: "pink" },
-    { id: 9, value: 12, color: "borrow" },
-    { id: 10, value: "Developers", color: "green" },
-  ];
+  // const objects = [
+  //   { id: 1, value: 1.2, color: "yellow" },
+  //   { id: 2, value: "Sasha", color: "white" },
+  //   { id: 3, value: "Kostya", color: "green" },
+  //   { id: 4, value: 244, color: "grey" },
+  //   { id: 5, value: 222, color: "yellow" },
+  //   { id: 6, value: "Pasha", color: "red" },
+  //   { id: 7, value: "Egor", color: "blue" },
+  //   { id: 8, value: 10.002, color: "pink" },
+  //   { id: 9, value: 12, color: "borrow" },
+  //   { id: 10, value: "Developers", color: "green" },
+  // ];
 
   const objectsCopy = [
     { id: 1, value: 1 },
@@ -28,42 +28,19 @@ export default function Square() {
     { id: 10, value: 6 },
   ];
 
-  return (
-    <div className="container">
+  function render(object) {
+    return (
       <div className="object">
-        {objects.map((el) => (
-          <div
-            className="square"
-            style={{ backgroundColor: el.color }}
-            key={el.id}
-          >
-            {el.value}
-          </div>
-        ))}
-      </div>
-      <div className="object">
-        {objectsCopy
+        {object
           .filter((obj) => obj.value >= 0 && obj.value <= 5)
           .map((filteredValue) => (
             <div className="square" style={{ backgroundColor: "yellow" }}>
               {filteredValue.value}
             </div>
           ))}
-        {objectsCopy
-          .filter((obj) => obj.value < 0)
-          .map((filteredValue) => (
-            <div className="square" style={{ backgroundColor: "red" }}>
-              {filteredValue.value}
-            </div>
-          ))}
-        {objectsCopy
-          .filter((obj) => obj.value > 5)
-          .map((filteredValue) => (
-            <div className="square" style={{ backgroundColor: "green" }}>
-              {filteredValue.value}
-            </div>
-          ))}
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <div className="container">{render(objectsCopy)}</div>;
 }
