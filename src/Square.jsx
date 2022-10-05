8ce2cbd8e2d652761a6f99519885ff80e359a006
kostos <putivcev123@gmail.com>
2022-10-05 21:36:11 +0300

    Задача доведена до конца
    Took 2 hours 43 minutes

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,74 +1,20 @@
 import React from "react";
 import "./App.css";
+import { render } from "./render";
 
 export default function Square() {
-  // const objects = [
-  //   { id: 1, value: 1.2, color: "yellow" },
-  //   { id: 2, value: "Sasha", color: "white" },
-  //   { id: 3, value: "Kostya", color: "green" },
-  //   { id: 4, value: 244, color: "grey" },
-  //   { id: 5, value: 222, color: "yellow" },
-  //   { id: 6, value: "Pasha", color: "red" },
-  //   { id: 7, value: "Egor", color: "blue" },
-  //   { id: 8, value: 10.002, color: "pink" },
-  //   { id: 9, value: 12, color: "borrow" },
-  //   { id: 10, value: "Developers", color: "green" },
-  // ];
-
   const objectsCopy = [
     { id: 1, value: 1 },
     { id: 2, value: 12 },
     { id: 3, value: -2 },
-    { id: 4, value: 1 },
+    { id: 4, value: 5 },
     { id: 5, value: -44 },
-    { id: 6, value: 0 },
+    { id: 6, value: 1112 },
     { id: 7, value: -11 },
     { id: 8, value: 2 },
-    { id: 9, value: 5 },
+    { id: 9, value: -22 },
     { id: 10, value: 6 },
   ];
-
-  function render(object) {
-    return (
-      <div className="object">
-        {object.map((filteredValue) => {
-          if (filteredValue.value >= 0 && filteredValue.value <= 5) {
-            return (
-              <div
-                className="square"
-                key={filteredValue.id}
-                style={{ backgroundColor: "yellow" }}
-              >
-                {filteredValue.value}
-              </div>
-            );
-          }
-          if (filteredValue.value < 0) {
-            return (
-              <div
-                className="square"
-                key={filteredValue.id}
-                style={{ backgroundColor: "red" }}
-              >
-                {filteredValue.value}
-              </div>
-            );
-          }
-          if (filteredValue.value > 5) {
-            return (
-              <div
-                className="square"
-                key={filteredValue.id}
-                style={{ backgroundColor: "green" }}
-              >
-                {filteredValue.value}
-              </div>
-            );
-          }
-        })}
-      </div>
-    );
-  }
 
   return <div className="container">{render(objectsCopy)}</div>;
 }
