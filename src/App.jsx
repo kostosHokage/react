46d8a6558c10e2128d6595ab38dcef9c58d32451
Konstantin <k.putivtsev@wizart.pro>
2022-10-07 23:45:07 +0300

    #12 Сделал наброски задания, осталось разобраться с выводом

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Square from "./Square";
+import Number from "./Number";
 
 function App() {
   return (
     <div className="App">
       <Square />
+      <Number />
     </div>
   );
 }
---
+++ src/Number.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Number = (num, func) => {
+   bitch = (num) => {
+    return <h1>{num * 2}</h1>
+  }
+}
+
+export default Number
