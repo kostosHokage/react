575a6d18eb8b879ca2cf14b37a58ea3759320e22
Konstantin <k.putivtsev@wizart.pro>
2022-10-10 21:59:10 +0300

    #12 Создал 5 функций, принимающие в себя разные числа, а так же условия

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./App.css";
-import Square from "./Square";
-import Number from "./Number";
+import AddNumber from "./AddNumber";
 
 function App() {
+  const fn = (x) => x + 5;
+  const fn1 = (x) => x * 10;
+  const fn2 = (x) => x + 223;
+  const fn3 = (x) => x + 1;
+  const fn4 = (x) => x / x;
+
   return (
     <div className="App">
-      <Square />
-      <Number />
+      <AddNumber num={3} func={fn} />
+      <AddNumber num={3.3} func={fn1} />
+      <AddNumber num={-31} func={fn2} />
+      <AddNumber num={2} func={fn3} />
+      <AddNumber num={100} func={fn4} />
     </div>
   );
 }
--- src/Number.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Number = (num, func) => {
-   bitch = (num) => {
-    return <h1>{num * 2}</h1>
-  }
-}
-
-export default Number
