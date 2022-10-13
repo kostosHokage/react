a3160e281e07f3a84fcd4371225ee5594f1e7b13
Konstantin <k.putivtsev@wizart.pro>
2022-10-13 23:10:45 +0300

    task is done

--- src/State.jsx
+++ src/State.jsx
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
 
 export default function State() {
-  const [state, setState] = useState(true);
+  const [state, setState] = useState(false);
   // eslint-disable-next-line no-console
   console.log(state);
-
   return (
     // eslint-disable-next-line react/button-has-type
-    <button onClick={() => setState(false)} className="button">
-      Click me, i show to u my state - {state}!
-    </button>
-
-    if (state === true) {
-      console.log('hello')
-    } else {
-      console.log('bye')
-    }
+    <div>
+      <button
+        onClick={() => {
+          if (!state) {
+            setState(true);
+          } else {
+            setState(false);
+          }
+        }}
+        className="button"
+      >
+        Click me, i show u box!
+      </button>
+      {!state ? <div className="square" /> : null}
+    </div>
   );
 }
