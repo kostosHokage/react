7bd26faa947f7b82a34473a40095ade19b32cc75
Konstantin <k.putivtsev@wizart.pro>
2022-11-01 21:53:47 +0300

    #18 закончил таску

--- src/State.jsx
+++ src/State.jsx
@@ -2,18 +2,16 @@ import React, { useEffect, useState } from "react";
 
 export default function State() {
   const [state, setState] = useState(false);
-  const [count, setCount] = useState(0);
-
-  const array = [];
+  const [count, setCount] = useState([]);
 
   useEffect(() => {
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < count; i++) {
-      array.push("Hello!");
-    }
-    // eslint-disable-next-line no-console
-    console.log(array);
-  });
+    setCount([...count, "hello!"]);
+    console.log(count);
+  }, [state]);
+
+  useEffect(() => {
+    console.log("я родился");
+  }, []);
 
   // eslint-disable-next-line no-console
 
@@ -22,13 +20,6 @@ export default function State() {
     <div>
       <button type="button" onClick={() => setState(!state)} className="button">
         Click me, i show u box!
-      </button>
-      <button
-        type="button"
-        onClick={() => setCount(count + 1)}
-        className="button"
-      >
-        Add new object to array
       </button>
       {!state ? <div className="square" /> : null}
     </div>
