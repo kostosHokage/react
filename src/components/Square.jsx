d38f7ad5e586999057ee85b670339750fab24e38
Konstantin <k.putivtsev@wizart.pro>
2022-11-17 01:15:13 +0300

    Добавил папку index.js в папку, где собраны все компоненте, тем самым убрал много лишнего в файле App.jsx

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "./App.css";
-import { render } from "./render";
+import "../App.css";
+import { render } from "../render";
 
 export default function Square() {
   const objectsCopy = [
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import AddNumber from "./AddNumber";
+import Square from "./Square";
+import State from "./State";
+import StateColor from "./StateColor";
+import StateSquare from "./StateSquare";
+
+export { AddNumber, Square, State, StateSquare, StateColor };
