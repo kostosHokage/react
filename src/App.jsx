895953b2bf615a552c6367ff307f155c4cf2ec13
Konstantin <k.putivtsev@wizart.pro>
2022-11-29 12:14:50 +0300

    #30 Исправил проблему с маршрутизацией проекта. Теперь при переходе по ссылке на наш проект, мы попадаем на страницу Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React, { StrictMode } from "react";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Generate from "./components/Generate";
 
 function App() {
   return (
     <React.StrictMode>
       <div className="App">
-        <BrowserRouter>
+        <HashRouter>
           <Generate />
-        </BrowserRouter>
+        </HashRouter>
       </div>
     </React.StrictMode>
   );
--- src/components/Generate.jsx
+++ src/components/Generate.jsx
@@ -7,14 +7,14 @@ export default function Generate() {
     <>
       <header className="header">
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <Link to="/Home">Home</Link>
+        <Link to="/">Home</Link>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <Link to="/Info">Info</Link>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <Link to="/OldTasks">Old Tasks</Link>
       </header>
       <Routes>
-        <Route path="/Home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/Info" element={<Info />} />
         <Route path="/OldTasks" element={<OldTasks />} />
         <Route path="/*" element={<NotFound />} />
